c7b94adcc08ff9b780a62b97ccbf44b3c8e6bd7a
cmendoza-dev <carlos.mendoza.s@edu.pe>
2023-09-29 00:07:34 -0500

    Base_de_datos

--- encabezado.php
+++ encabezado.php
@@ -5,10 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Conexión de PHP con PostgreSQL usando PDO">
-    <meta name="author" content="Odenfis">
+    <meta name="author" content="cmendoza-dev">
     <title>PostgreSQL-PHP | Por Carlos Mendoza</title>
     <link href="./bootstrap.min.css" rel="stylesheet">
-    <link href="./style.css" rel="stylesheet" type="text/css">
+    <link href="./style.css" rel="stylesheet">
 </head>
 
 <body>
