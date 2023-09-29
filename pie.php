ec62771deeafb021153260931f2187f4814dbbd5
cmendoza-dev <carlos.mendoza.s@edu.pe>
2023-09-29 15:18:21 -0500

    Algunos cambios

--- pie.php
+++ pie.php
@@ -10,19 +10,19 @@ Este archivo simplemente cierra las etiquetas abiertas en encabezado.php
         <ul>
             <li> <img src="images/friendly-pets.jpg" width="240" height="186" alt="">
                 <h2><a href="#"></a></h2>
-                <p> Lorem ipsum dolor sit amet, consectetuer adepiscing elit, sed diam nonummy nib. <a class="more" href="#">Read More</a> </p>
+                <p> Lorem ipsum dolor sit amet, consectetuer adepiscing elit, sed diam nonummy nib. <a class="more" href="#">Leer Más</a> </p>
             </li>
             <li> <img src="images/pet-lover2.jpg" width="240" height="186" alt="">
                 <h2><a href="#"></a></h2>
-                <p> Lorem ipsum dolor sit amet, cons ectetuer adepis cing, sed diam euis. <a class="more" href="#">Read More</a> </p>
+                <p> Lorem ipsum dolor sit amet, cons ectetuer adepis cing, sed diam euis. <a class="more" href="#">Leer Más</a> </p>
             </li>
             <li> <img src="images/healthy-dog.jpg" width="240" height="186" alt="">
                 <h2><a href="#"></a></h2>
-                <p> Lorem ipsum dolor sit amet, consectetuer adepiscing elit, sed diam nonu mmy. <a class="more" href="#">Read More</a> </p>
+                <p> Lorem ipsum dolor sit amet, consectetuer adepiscing elit, sed diam nonu mmy. <a class="more" href="#">Leer Más</a> </p>
             </li>
             <li>
                 <h2><a href="#"></a></h2>
-                <p> Lorem ipsum dolor sit amet, consectetuer adepiscing elit, sed diameusim. <a class="more" href="#">Read More</a> </p>
+                <p> Lorem ipsum dolor sit amet, consectetuer adepiscing elit, sed diameusim. <a class="more" href="#">Leer Más</a> </p>
                 <img src="images/pet-lover.jpg" width="240" height="186" alt="">
             </li>
         </ul>
