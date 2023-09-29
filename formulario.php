5ca89e24e019b9f3151386f6226e001fde691a74
cmendoza-dev <carlos.mendoza.s@edu.pe>
2023-09-28 23:34:14 -0500

    Base_de_datos

--- formulario.php
+++ formulario.php
@@ -3,22 +3,24 @@
 Este archivo muestra un formulario que se envía a insertr.php, el cual guardará los datos
 */
 ?>
-<?php include_once 'encabezado.php'?>
+<?php include_once 'encabezado.php' ?>
 <div class="row">
     <div class="col-12">
         <h1>Agregar</h1>
-        <form action="insertar.php" method="POST">
-            <div class="form-group">
-                <label for="nombre">Nombre</label>
-                <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre de mascota" required>
-            </div>
-            <div class="form-group">
-                <label for="edad">Edad</label>
-                <input type="number" class="form-control" id="edad" name="edad" placeholder="Edad de mascota" required>
-            </div>
-            <button type="submit" class="btn btn-success">Guardar</button>
-            <a href="./listar.php" class="btn btn-warning">Ver todas</a>
-        </form>
+        <div class="justify-content-center align-items-center">
+            <form action="insertar.php" method="POST" class="w-50 ">
+                <div class="form-group">
+                    <label for="nombre">Nombre</label>
+                    <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre de mascota" required>
+                </div>
+                <div class="form-group">
+                    <label for="edad">Edad</label>
+                    <input type="number" class="form-control" id="edad" name="edad" placeholder="Edad de mascota" required>
+                </div>
+                <button type="submit" class="btn btn-success">Guardar</button>
+                <a href="./listar.php" class="btn btn-warning">Ver todas</a>
+            </form>
+        </div>
     </div>
 </div>
-<?php include_once 'pie.php'?>
+<?php include_once 'pie.php' ?>
